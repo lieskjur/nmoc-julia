f12a410ec9a554a928614c59881f597dc9a54a31
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2024-02-18 11:47:49 +0100

    car import problem

---
+++ src/car_import.jl
@@ -0,0 +1,17 @@
+using JuMP
+using HiGHS
+
+model = JuMP.Model(HiGHS.Optimizer)
+
+@variable(model, x[1:2] >= 0)
+@constraints(model, begin
+    x[1] * 2.5 + x[2] * 0.5 <= 50
+    x[1] + x[2] <= 60
+end)
+@objective(model, Max, x[1] * 0.25 + x[2] * 0.075)
+
+JuMP.optimize!(model)
+
+println("x = ", value.(x))
+println("c'x = ", objective_value(model))
+
